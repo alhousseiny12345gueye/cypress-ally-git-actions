a8344f707d0c3191e8a255587ea3bf70dc4a7d71
unknown <alsenygueye@gmail.com>
2024-05-27 13:13:42 +0200

    setting up cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     "baseUrl": "http://localhost:3000",
     "viewportHeight": 1000,
     "viewportWidth": 1280,
-    projectId: "61srdj",
+    projectId: "iw47ve",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
