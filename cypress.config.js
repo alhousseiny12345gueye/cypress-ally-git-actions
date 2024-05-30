d7d04073380229ebefe3e75e41baaef29c61a9b4
unknown <alsenygueye@gmail.com>
2024-05-30 12:19:59 +0200

    add index.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     "baseUrl": "http://localhost:3000",
+    "pluginsFile": "cypress/plugins/index.js",
     "viewportHeight": 1000,
     "viewportWidth": 1280,
     projectId: "iw47ve",
